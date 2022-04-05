15a1168eabec67c018d1d4cb0c55c7fe0a0b4a08
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-04-05 21:53:29 +0300

    add products and product components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
+import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 
@@ -13,6 +14,7 @@ const Home = () => {
       <Navbar />
       <Slider />
       <Categories />
+      <Products/>
       <Newsletter/>
       <Footer/>
     </div>
