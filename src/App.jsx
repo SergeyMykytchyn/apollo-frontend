cc8618f7c7d0e05c7371afaf2ccc56901bd95a2b
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-04-05 18:15:36 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
+import { useSelector } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Success from "./pages/Success";
 
 const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
+  
   return (
-    <div>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+        <Route path="/login">
+          {user ? <Navigate to="/" /> : <Login />}
+        </Route>
+        <Route path="/register">
+          {user ? <Navigate to="/" /> : <Register />}
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
