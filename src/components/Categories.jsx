28b57b2fb4bcde2c23c07c4aa885eee98773c6ed
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-04-05 21:19:30 +0300

    add newsletter component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import CategoryItem from "./CategoryItem";
 import { categories } from "../data";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Navbar />
       <Slider />
       <Categories />
+      <Newsletter/>
     </div>
   );
 };
